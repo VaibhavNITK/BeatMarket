f88ae3f19c7ff0146d569ba61b55e4d2847c22e7
Vaibhav <vaibhavagarwal693@gmail.com>
2024-02-18 13:23:54 +0530

    initial backend configuration

---
+++ api/index.js
@@ -0,0 +1,18 @@
+// import {app} from "./app.js";
+import express from "express";
+import { connectDB } from "./data/database.js"; 
+
+const app = express();
+
+connectDB(); 
+
+
+app.get('/', (req, res) => {
+  console.log(req.method); 
+  res.send(`Hello World! The request method is ${req.method}`); 
+});
+
+// Start the server
+app.listen(4000, () => {
+  console.log("Server is listening on port 4000"); 
+});
