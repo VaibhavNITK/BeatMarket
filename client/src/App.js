eebec3385b67cd2e81b6ef550c1bae3b649134ec
Aditya <adityachauhan2501@gmail.com>
2024-02-18 13:26:37 +0530

    navbar-done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Producers from "./pages/Producers";
 
-
-function App() {
+export default function App() {
   return (
-    <div className='text-center'>Hello</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contacts" element={<Contacts />} />
+        <Route path="producers" element={<Producers />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
